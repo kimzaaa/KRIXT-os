33c16b8d061ad37fe0650e529861be396940ba2f
kimzaaa <kimzamusic69@gmail.com>
2022-09-26 21:43:37 +0700

    Krixt os 1.2.0.0-NfR-2.0-G-R

--- krixt os.py
+++ krixt os.py
@@ -38,8 +38,10 @@ class Window(QMainWindow,QWidget):
         self.uih = QLabel(" ",self)
         self.cb = Button(" ", self)
         self.bb = QPushButton(" ",self)
+        self.nb = QPushButton(" ",self)       
         self.nauih = QLabel(" ",self)
         self.note = QPlainTextEdit(" ",self)
+        self.cnb = QPushButton(" ",self)
       
         self.button = QPushButton(" ", self)
         self.button.setGeometry(460,950,1000,10)
@@ -54,21 +56,29 @@ class Window(QMainWindow,QWidget):
         self.cb.setVisible(False)
         self.bb.setVisible(False)
         self.uih.setVisible(False)
+        self.nauih.setVisible(False)
+        self.note.setVisible(False)
+        self.nb.setVisible(False)
+        self.cnb.setVisible(False)
         self.button.clicked.connect(self.doAnimation)
         self.button2.clicked.connect(self.dostopAnimation)
 
         #self.uih.setGeometry(380,100,1180,820)
         self.cb.setFixedSize(100,100)
         self.bb.setFixedSize(100,100)
+        self.nb.setFixedSize(100,100)
         self.nauih.setFixedSize(435,220)
         self.note.setFixedSize(410,195)
+        self.cnb.setFixedSize(25,25)
 
         self.cb.setStyleSheet("background-image : url(C:/Users/kim/Desktop/coding/KRIXT-os/calcmedlogov6.png); border-radius : 10")
         self.bb.setStyleSheet("background-image : url(C:/Users/kim/Desktop/coding/KRIXT-os/kiconv3.png); border-radius : 10")
+        self.nb.setStyleSheet("background-image : url(C:/Users/kim/Desktop/coding/KRIXT-os/nicon.png); border-radius : 10")
         self.uih.setStyleSheet("background-image : url(C:/Users/kim/Desktop/coding/KRIXT-os/rbg3.png); border-radius : 20")
         self.nauih.setStyleSheet("background-image : url(C:/Users/kim/Desktop/coding/KRIXT-os/rbg3.png); border-radius : 20")
         self.note.setStyleSheet("background-image : url(C:/Users/kim/Desktop/coding/KRIXT-os/rbg3.png); border: none; color : white; font-size : 13pt") 
         self.note.setFont(font)
+        self.cnb.setStyleSheet("background-image : url(C:/Users/kim/Desktop/coding/KRIXT-os/sxc.png); border : none")
         self.opacity_effect = QGraphicsOpacityEffect()
         self.opacity_effect.setOpacity(0.6)
         self.uih.setGraphicsEffect(self.opacity_effect)
@@ -76,12 +86,15 @@ class Window(QMainWindow,QWidget):
         
         self.cb.clicked.connect(self.click)
         self.bb.clicked.connect(self.openbrowser)
+        self.nb.clicked.connect(self.opennotes)
+        self.cnb.clicked.connect(self.closenotes)
 
         #self.uih.move(380,100)
         #self.cb.move(400,124)
         #self.bb.move(510,124)
         self.nauih.move(1110,680)
-        self.note.move(1120,690)
+        self.note.move(1135,690)
+        self.cnb.move(1120,690)
 
 
         self.btn_close = QPushButton(" ",clicked = self.closecalc)
@@ -186,14 +199,17 @@ class Window(QMainWindow,QWidget):
         self.anim = QPropertyAnimation(self.uih, b"geometry")
         self.anim2 = QPropertyAnimation(self.cb, b"geometry")
         self.anim3 = QPropertyAnimation(self.bb, b"geometry")
+        self.anim4 = QPropertyAnimation(self.nb, b"geometry")
 
         self.cb.setVisible(True)
         self.bb.setVisible(True)
         self.uih.setVisible(True)
+        self.nb.setVisible(True)
 
         self.anim.setDuration(300)
         self.anim2.setDuration(300)
         self.anim3.setDuration(300)
+        self.anim4.setDuration(300)
 
         self.anim.setStartValue(QRect(380,1000,1180,820))
         self.anim.setEndValue(QRect(380,100,1180,820))
@@ -204,9 +220,13 @@ class Window(QMainWindow,QWidget):
         self.anim3.setStartValue(QRect(510,1000,100,100))
         self.anim3.setEndValue(QRect(510,120,100,100))
 
+        self.anim4.setStartValue(QRect(620,1000,100,100))
+        self.anim4.setEndValue(QRect(620,120,100,100))
+
         self.anim.start()
         self.anim2.start()
         self.anim3.start()
+        self.anim4.start() 
 
         
 
@@ -216,14 +236,17 @@ class Window(QMainWindow,QWidget):
         self.anim = QPropertyAnimation(self.uih, b"geometry")
         self.anim2 = QPropertyAnimation(self.cb, b"geometry")
         self.anim3 = QPropertyAnimation(self.bb, b"geometry")
+        self.anim4 = QPropertyAnimation(self.nb, b"geometry")
 
         self.cb.setVisible(True)
         self.bb.setVisible(True)
         self.uih.setVisible(True)
+        self.nb.setVisible(True)
 
         self.anim.setDuration(300)
         self.anim2.setDuration(300)
         self.anim3.setDuration(300)
+        self.anim4.setDuration(300)
 
         self.anim.setStartValue(QRect(380,100,1180,820))
         self.anim.setEndValue(QRect(380,1000,1180,820))
@@ -234,9 +257,13 @@ class Window(QMainWindow,QWidget):
         self.anim3.setStartValue(QRect(510,120,100,100))
         self.anim3.setEndValue(QRect(510,1000,100,100))
 
+        self.anim4.setStartValue(QRect(620,120,100,100))
+        self.anim4.setEndValue(QRect(620,1000,100,100))
+
         self.anim.start()
         self.anim2.start()
         self.anim3.start()
+        self.anim4.start()
 
     def num_press(self,key_number):
         self.temp_nums.append(key_number)
@@ -280,6 +307,17 @@ class Window(QMainWindow,QWidget):
         
     def openbrowser(self):
         os.system("C:/Users/kim/Desktop/coding/KRIXT-os/browser/browser.exe")
+
+    def opennotes(self):
+        self.nauih.setVisible(True)
+        self.note.setVisible(True)
+        self.cnb.setVisible(True)
+
+    def closenotes(self):
+        self.nauih.setVisible(False)
+        self.note.setVisible(False)
+        self.cnb.setVisible(False)
+
 
     
 
